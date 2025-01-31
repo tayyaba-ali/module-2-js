625d1dc508d2e9be9b8291a730c8fe7cb43e96ef
Umra-Badar20 <umrabadar555@gmail.com>
2025-01-31 13:07:41 +0500

    var vs let vs const

---
+++ app.js
@@ -0,0 +1,28 @@
+// alert("kuch bh ");
+// ("kuch bh ");
+// alert("kuch bh ");
+// prompt("kuch bh ");
+// alert("kuch bh ");
+// alert("kuch bh ");
+// alert("kuch bh ");
+// console.log("<h1>hi from html</h1>");
+// document.write("<h1>hi from html</h1>")
+
+
+// let userName = "kfdskhfd"
+// // hellohe = "replace";
+
+// userName = "tayyaba"
+// console.log(userName);
+
+// const pi = 3.147;
+// pi = 44445
+// console.log(pi);
+
+// new trick 
+// pascal name  
+
+// myName = "Tayyaba"
+// console.log(myName);
+
+// assigment 
