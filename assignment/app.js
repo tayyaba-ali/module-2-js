a6872fe4de781540f99c1817917d8ecc30c878e7
tayyaba-ali <ta6402792@gmail.com>
2025-02-08 00:24:44 +0500

    if else

---
+++ assignment/app.js
@@ -0,0 +1,116 @@
+// console.log(myName);
+
+// let myName ;
+// console.log(myName);
+
+// let num = 0;
+// // num = num+1
+// // num++
+// num += 1 //shorthand for increment
+// console.log(num);
+
+// let userTableNum = +prompt("enter your number");
+
+// let counter = 1;
+
+// if (userTableNum == 0) {
+//   for (let i = counter; i <= 10; i++) {
+//     document.write("5" + "X " + i + "=" + 5 * i + "<br />");
+//   }
+// } else {
+//   for (let i = counter; i <= 10; i++) {
+//     document.write(userTableNum + "X " + i + "=" + userTableNum * i + "<br />");
+//   }
+// }
+
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+// counter++
+// document.write("4 X "+ counter+"=" + 4*counter+"<br />")
+
+// temperature
+
+// let celsius = 25;
+
+// let convertedInToFarenheit = (celsius * 9) / 5 + 32;
+
+// document.write(celsius + "°C " + "is " + convertedInToFarenheit);
+// console.log(convertedInToFarenheit);
+
+// let sub1 =prompt("what's your subject name","english")
+// let sub2 =prompt("what's your subject name","urdu")
+// let sub3 =prompt("what's your subject name","maths")
+// const total = 300
+// let sub1ObtainedMarks =  +prompt("what's your subject numbers")
+// let sub2ObtainedMarks =  +prompt("what's your subject numbers")
+// let sub3ObtainedMarks =  +prompt("what's your subject numbers")
+
+// let percentage = ((sub1ObtainedMarks+sub2ObtainedMarks+sub3ObtainedMarks)/total)*100
+
+// console.log(percentage);
+
+// if(true){
+// console.log("running");
+
+// }
+
+// console.log("cat" < "car");
+
+// console.log("zubarya".charCodeAt());
+
+// 74<72
+
+// console.log(true && true && false && true);
+// console.log(Boolean(""));
+
+// ""
+// 0
+// undefine!d
+// null
+// NaN
+// false
+// -0
+
+// let isOnline = true;
+// if(""){
+//     console.log("offline hojao")
+// }
+
+// let userName = ""
+
+let password = 1234;
+
+let userPass = +prompt("password");
+// if(password === userPass){
+//     console.log("access granted");
+
+// }
+// else{
+//     console.log("apna wifi lagalo");
+
+// }
+
+if (password !== userPass) {
+  if (userPass == "") {
+    console.log("write something");
+  } else {
+    console.log("apna wifi lagalo");
+  }
+} else {
+  console.log("use krlo");
+}
